b62413803a4b02f342b0a1da35f6477e056de550
Jhonatan Oliveira <Jhonatandev0@gmail.com>
2021-08-17 21:10:59 -0300

    Criando tabelas usando o PHP
    Criando tabelas utilizando o php

--- index.php
+++ index.php
@@ -7,3 +7,17 @@ $senha = '';
 $database = 'jhonatan';
 
 $conexao = mysqli_connect($servidor,$usuario,$senha,$database);
+
+# Criando tabelas usando PHP
+# Tabela cursos (nome do curso, carga horaria)
+$query = 'CREATE TABLE CURSOS (
+	id int not null auto_increment,
+	nome_curso varchar(255) not null,
+	carga_horaria int not null,
+	primary key(id)
+)';
+
+$executar = mysqli_query($conexao,$query);
+# Tabela alunos (nome do aluno, data_nascimento)
+# Tabela alunos_cursos (aluno, curso)
+
