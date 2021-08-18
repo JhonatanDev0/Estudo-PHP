9734dd981ed6f15e922c9a9ddd69ff890a7c1a91
Jhonatan Oliveira <Jhonatandev0@gmail.com>
2021-08-17 21:04:08 -0300

    Conectando ao banco
    Conectando ao banco MySQL

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+# Conexão com o banco de dados MySQL
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$database = 'jhonatan';
+
+$conexao = mysqli_connect($servidor,$usuario,$senha,$database);
