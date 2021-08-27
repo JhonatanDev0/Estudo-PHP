9c802404675b86b26e75787fff46d403dd372227
Jhonatan Oliveira <Jhonatandev0@gmail.com>
2021-08-26 21:27:38 -0300

    Atualizando de if para switch

--- index.php
+++ index.php
@@ -13,22 +13,15 @@ $pagina = $_GET['pagina'];
     $pagina = "home";
 }
 
-if($pagina == 'cursos'){
-    include 'views/cursos.php';
-}elseif($pagina == 'alunos'){
-    include 'views/alunos.php';
-}elseif($pagina == 'matriculas'){
-    include 'views/matriculas.php';
-}elseif($pagina == 'inserir_curso'){
-    include 'views/inserir_curso.php';
-}elseif($pagina == 'inserir_aluno'){
-    include 'views/inserir_aluno.php';
-}elseif($pagina == 'inserir_matricula'){
-    include 'views/inserir_matricula.php';
-}else{
-    include 'views/home.php';
+switch($pagina){
+    case 'cursos': include 'views/cursos.php'; break;
+    case 'alunos': include 'views/alunos.php'; break;
+    case 'matriculas': include 'views/matriculas.php'; break;
+    case 'inserir_aluno': include 'views/inserir_aluno.php'; break;
+    case 'inserir_matricula': include 'views/inserir_matricula.php'; break;
+    case 'inserir_curso': include 'views/inserir_curso.php'; break;
+    default: include 'views/home.php';
 }
-
 
 # Rodapé
 include 'footer.php';
