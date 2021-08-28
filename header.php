b5ae802a73ab94a627cfd1afc105555716eb08d2
Jhonatan Oliveira <Jhonatandev0@gmail.com>
2021-08-28 20:17:12 -0300

    Utilizando componentes do bootstrap

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
     <head>
         <title>OS GEO</title>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
 
